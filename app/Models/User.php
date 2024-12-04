bf68d86e2bfc8684f7ffdee8156360172712c27f
unthenamed <unthenamed@gmail.com>
2024-12-05 02:16:33 +0700

    f

---
+++ app/Models/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model\Model;
+use Core\Valid\Hash;
+
+final class User extends Model
+{
+    protected $table = 'users';
+
+    protected $primaryKey = 'id';
+
+    protected $typeKey = 'int';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'access_key',
+        'is_filter',
+        'can_edit',
+        'can_delete',
+        'can_reply',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_filter' => 'bool',
+        'can_edit' => 'bool',
+        'can_delete' => 'bool',
+        'can_reply' => 'bool',
+        'is_active' => 'bool',
+    ];
+
+    public function __construct(array $data = [])
+    {
+        $this->fill($data);
+    }
+
+    protected function fakes(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => Hash::make(fake()->text(8)),
+        ];
+    }
+}
